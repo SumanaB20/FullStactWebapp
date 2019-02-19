9a6b07263fd33336f3b17077d857b2411878f3f1
Sumana B <sumana18bee@gmail.com>
2019-02-19 14:20:20 +0530

    proxy settings

--- config/prod.js
+++ config/prod.js
@@ -4,7 +4,3 @@ module.exports ={
   mongoURI:process.env.MONGO_URI,
   cookieKey:process.env.COOKIE_KEY
 };
-
-
-//414943867493-ele5ugc0igjsogrlk1jccf75pdq0eeae.apps.googleusercontent.com
-//lKjEdZV16QbKuqOu8Tl9Ske3
