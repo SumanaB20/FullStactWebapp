df48b613578048fc21e71c59ed7fd5d3cc1b877d
Sumana B <sumana18bee@gmail.com>
2019-02-19 14:17:33 +0530

    Auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports ={
+  googleClientID:process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI:process.env.MONGO_URI,
+  cookieKey:process.env.COOKIE_KEY
+};
+
+
+//414943867493-ele5ugc0igjsogrlk1jccf75pdq0eeae.apps.googleusercontent.com
+//lKjEdZV16QbKuqOu8Tl9Ske3
--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+require('./models/User')
+require('./services/passport')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const key = require('./config/key')
+
 const app = express()
+require('./routes/authRoutes')(app)
 
-app.get('/',(req,res)=>{
-  res.send({ hi:'there'})
-})
+mongoose.connect(key.mongoURI)
 
-app.listen(5000)
+app.use(cookieSession({
+  maxAge:30*24*60*60*1000,
+  keys:[key.cookieKey]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
